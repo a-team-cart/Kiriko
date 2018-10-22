9756c55403a47ee8c9217ffc0a3a8d6b9ab108c1
Codrin Tablan Negrei <kodrintxps@gmail.com>
2018-10-22 01:17:18 -0400

    started UI variables

---
+++ Assets/Scripts/UIBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //for using the UI
+
+public class UIBehaviour : MonoBehaviour {
+
+//UI variables
+
+//gravity
+public float maxGravity;
+public float minGravity;
+
+//instatiated/destroyed objects
+public int maxObjects;
+public int minObjects;
+public int[] objects;
+
+// player variables;
+public float playerXPos;
+public float playerYPos;
+public float playerRadius;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
