53ea658a82592374f9a657b31a715d4d8109657f
charlesDouc <charles.douc@gmail.com>
2018-10-29 15:58:37 -0400

    Added rotate effect to the scene. Adjust the skybox. Reorganize greybox scene files.

---
+++ Assets/Scripts/GreyBox/rotateObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateObject : MonoBehaviour {
+
+	// public variables ---------------------
+	public float m_speed;
+
+	// private variables --------------------
+
+	// --------------------------------------
+	// Use this for initialization
+	// --------------------------------------
+	void Start () {
+		
+	}
+	
+	// --------------------------------------
+	// Update is called once per frame
+	// --------------------------------------
+	void Update () {
+		gameObject.transform.Rotate(0, m_speed, 0);
+		
+	}
+
+	// --------------------------------------
+	// Methods
+	// --------------------------------------
+}
