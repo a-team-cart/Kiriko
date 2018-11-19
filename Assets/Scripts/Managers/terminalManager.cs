e49ed4a871917152b4418cd0fa4055551cf0172a
charlesDouc <charles.douc@gmail.com>
2018-11-19 15:03:38 -0500

    Cleaned up old assets. Imported the UI and the assets needed for it

---
+++ Assets/Scripts/Managers/terminalManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class terminalManager : MonoBehaviour {
+	// public variables --------------------
+	public GameObject m_InputManager;
+	[Header("Sliders Value")]
+	public GameObject[] m_effects;
+
+
+	// private variables -------------------
+
+
+	// -------------------------------------
+	// Use this for initialization
+	// -------------------------------------
+	void Start () {
+		
+	}
+	
+	// -------------------------------------
+	// Update is called once per frame
+	// -------------------------------------
+	void Update () {
+		// Update values of sliders
+		catchSliderValues();		
+
+		
+	}
+
+	// -------------------------------------
+	// Methods
+	// -------------------------------------
+	private void catchSliderValues() {
+		// Screen the values from input manager
+		m_effects[0].GetComponent<Slider>().value = m_InputManager.GetComponent<inputManager>().m_saturation;
+		m_effects[1].GetComponent<Slider>().value = m_InputManager.GetComponent<inputManager>().m_gravity;
+		m_effects[2].GetComponent<Slider>().value = m_InputManager.GetComponent<inputManager>().m_timeSpeed;
+		m_effects[3].GetComponent<Slider>().value = m_InputManager.GetComponent<inputManager>().m_lightIntensity;
+		m_effects[4].GetComponent<Slider>().value = m_InputManager.GetComponent<inputManager>().m_soundPitch;
+		m_effects[5].GetComponent<Slider>().value = m_InputManager.GetComponent<inputManager>().m_normalIntensity;
+	}
+
+
+	private void showDescriptions() {
+		
+
+
+	}
+}
---
+++ Assets/Scripts/Terminal/effectDescriptionController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effectDescriptionController : MonoBehaviour {
+
+	// public variables --------------------
+	public RectTransform m_text;
+	public RectTransform m_line;
+	[Space(10)]
+	public float m_animationSpeed = 15f;
+	public float m_textMaxWidth = 160f;
+	public float m_lineMaxWidth = 235f;
+	public bool m_activate;
+
+	// private variables -------------------
+	private float m_textWidth;
+	private float m_lineWidth;
+
+
+	// -------------------------------------
+	// Use this for initialization
+	// -------------------------------------
+	void Start () {
+		// Disable descriptions
+		m_textWidth = 0f;
+		m_lineWidth = 0f;
+
+		// Update changes
+		m_text.sizeDelta = new Vector2 (m_textWidth, 76);
+		m_line.sizeDelta = new Vector2 (m_lineWidth, 1);
+		
+	}
+	
+	// -------------------------------------
+	// Update is called once per frame
+	// -------------------------------------
+	void Update () {
+		// Start activation animation
+		if (m_activate)
+			activateDescription();
+
+		// Start closing animation
+		if (!m_activate)
+			desableDescription();
+		
+	}
+
+	// -------------------------------------
+	// Methods
+	// -------------------------------------
+	private void activateDescription() {
+
+		// Change the width of the line
+		if (m_lineWidth < m_lineMaxWidth) {
+			m_lineWidth += m_animationSpeed;
+			
+			// Update changes
+			m_line.sizeDelta = new Vector2 (m_lineWidth, 1);
+		}
+
+		// Change the width of the text
+		if (m_textWidth < m_textMaxWidth) {
+			m_textWidth += m_animationSpeed;
+
+			// Update changes
+			m_text.sizeDelta = new Vector2 (m_textWidth, 76);
+		}
+
+	}
+
+
+	private void desableDescription() {
+
+		// Change the width of the line
+		if (m_lineWidth > 0f) {
+			m_lineWidth -= m_animationSpeed;
+			
+			// Update changes
+			m_line.sizeDelta = new Vector2 (m_lineWidth, 1);
+		}
+
+		// Change the width of the text
+		if (m_textWidth > 0f) {
+			m_textWidth -= m_animationSpeed;
+
+			// Update changes
+			m_text.sizeDelta = new Vector2 (m_textWidth, 76);
+		}
+
+	}
+
+
+}
