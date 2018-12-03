cca7694660409ef1cdf5b0a4749387b3206d823c
Codrin Tablan Negrei <kodrintxps@gmail.com>
2018-12-02 19:37:17 -0500

    Audio man. and Terminal replace

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -20,19 +20,12 @@ public class AudioManager : MonoBehaviour {
 
     //Create string with all relevant audio
     //arameters for max to parse on its side.
-    //--------------
-    // Sound-prefixed variables are self-explanatory
-    // Light intensity: Intensity of noise (pink/white or analog)
-    // Light saturation: Distortion of noise (overdrive or distort)
-    // Object gravity: Pitch slide of selected samples
-    // Object scale: Size of reverb space
-    //--------------
     private string serializeAudioParams()
     {
         var formattedString = 
             String.Format("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}", 
             _inputMan.m_soundPitch, _inputMan.m_soundTimescale, _inputMan.m_soundReverbDecay, _inputMan.m_soundFeedback, _inputMan.m_soundFilter, _inputMan.m_soundSlide,
-            _inputMan.m_lightIntensity, _inputMan.m_lightSaturation, _inputMan.m_objectGravity, _inputMan.m_objectScale);
+            _inputMan.m_PPsaturation, _inputMan.m_PPchromaticAberration, _inputMan.m_PPvignette, _inputMan.m_objectScale);
         return formattedString;
     }
 }
---
+++ Assets/Scripts/Terminal/textBlinking.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textBlinking : MonoBehaviour {
+
+	// public variables ---------------------
+	public float m_timeLimit;
+	public GameObject m_textBlink;
+
+	// private variables --------------------
+	private float m_counter;
+	private bool m_showText;
+
+	// --------------------------------------
+	// Use this for initialization
+	// --------------------------------------
+	void Start () {
+		m_counter = 0f;
+		m_showText = true;
+		
+	}
+	
+	// --------------------------------------
+	// Update is called once per frame
+	// --------------------------------------
+	void Update () {
+		m_counter += Time.deltaTime;
+
+		if (m_counter > m_timeLimit) {
+			m_showText = !m_showText;
+			m_counter = 0f;
+		}
+
+		if (m_showText)
+			m_textBlink.SetActive(true);
+
+		if (!m_showText)
+			m_textBlink.SetActive(false);
+
+		
+	}
+
+	// --------------------------------------
+	// Update is called once per frame
+	// --------------------------------------
+}
